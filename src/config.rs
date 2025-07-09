781fb2548e1104e797abe4e2c0fed436ed344a95
xingxq <xingxq@yeah.net>
2025-07-09 16:25:02 +0800

    修改ID、终极服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["117.72.122.86"];
+pub const RS_PUB_KEY: &str = "p4F3lCe8C+ur5v2mtXx3Ebdm6kc9rpdpNCVEjoE2MkY=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
